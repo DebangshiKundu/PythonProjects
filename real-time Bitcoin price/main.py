91cd75110682eb321e7b49f25cb8d12873d0f464
DebangshiKundu <debangshikundu03@gmail.com>
2024-01-16 13:37:32 +0530

    web scraping

--- real-time Bitcoin price/main.py
+++ real-time Bitcoin price/main.py
@@ -6,16 +6,17 @@ from playsound import playsound
 def scrape():
     url='https://coinmarketcap.com/currencies/bitcoin/'
 
-    resp=requests.get(url)
+    r=requests.get(url)
 
-    soup=BeautifulSoup(resp.text,'lxml')
+    soup=BeautifulSoup(r.text,'lxml')
     current=soup.body.find(class_='sc-f70bb44c-0 jxpCgO base-text')
 
+    print(current.text)
     return current.text.replace('$','').replace(',','')
 
 target=50000
 while True:
-    time.sleep(2)
+    time.sleep(5)
     current=float(scrape())
     if target>=current:
         playsound('pythonProject/real-time Bitcoin price/notification.wav')
